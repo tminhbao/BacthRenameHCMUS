53ed68d5cd705428dddc677a8d95eaa12a55069d
tminhbao <tminhbao2001@gmail.com>
2021-12-20 22:53:20 +0700

    Fix UI

--- BatchRename/BatchRename/MainWindow.xaml.cs
+++ BatchRename/BatchRename/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+
+
 namespace BatchRename
 {
     /// <summary>
@@ -24,5 +29,35 @@ namespace BatchRename
         {
             InitializeComponent();
         }
+
+        class Filename : INotifyPropertyChanged
+        {
+            public int id { get; set; }
+            public string oldName { get; set; }
+            public string filePath { get; set; }
+            public string newName { get; set; }
+            public string err { get; set; }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileTab.Items.Refresh();
+            FolderTab.Items.Refresh();
+        }
+
+
+        public string Path { get; set; }
+        private void AddFileButtons_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            if(openFileDialog.ShowDialog()==true)
+            {
+                Path = openFileDialog.FileName;
+                MessageBox.Show(Path);
+            }
+        }
+        
     }
 }
